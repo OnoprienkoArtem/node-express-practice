1e66066c4a2ed5fe572efee05662558ece15efda
OnoprienkoArtem <o.artem.e@gmail.com>
2021-01-17 20:10:08 +0200

    add static html

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
+app.get('/', (req, res) => {
+  res.status(200); // added automatically
+  res.sendfile(path.join(__dirname, 'views', 'index.html'));
+});
 
+app.get('/about', (req, res) => {
+  res.sendfile(path.join(__dirname, 'views', 'about.html'));
+});
 
 
 
