2f3a663424a112289eff067443a2e6f87a28c396
OnoprienkoArtem <o.artem.e@gmail.com>
2021-01-17 20:27:21 +0200

    setup handlebars

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express');
-const path = require('path');
+const exphbs = require('express-handlebars');
+
 const app = express();
 
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'pages');
+
+
 app.get('/', (req, res) => {
-  res.status(200); // added automatically
-  res.sendfile(path.join(__dirname, 'views', 'index.html'));
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  res.sendfile(path.join(__dirname, 'views', 'about.html'));
+  res.render('about');
 });
 
 
