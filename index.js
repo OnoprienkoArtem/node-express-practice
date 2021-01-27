cc647718f09885bcfa244202bafd430c8881a42d
OnoprienkoArtem <o.artem.e@gmail.com>
2021-01-27 22:56:44 +0200

    work with deleting item from cart

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.engine('hbs', hbs.engine);
 app.set('view engine', 'hbs');
 app.set('views', 'pages');
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({extended: true}));
 
--- public/app.js
+++ public/app.js
@@ -4,3 +4,13 @@ document.querySelectorAll('.price').forEach(node => {
     style: 'currency'
   }).format(node.textContent);
 });
+
+const $cart = document.querySelector('#cart');
+if ($cart) {
+  $.cart.addEventListener('click', event => {
+    if (event.target.classList.contains('js-remove')) {
+      const id = event.target.dataset.id;
+      console.log(id)
+    }
+  })
+}
