8f0db75535aaee3bfc3c5c80d99708099bda7323
OnoprienkoArtem <o.artem.e@gmail.com>
2021-02-12 23:22:30 +0200

    route for order

--- routes/orders.js
+++ routes/orders.js
@@ -2,11 +2,25 @@ const {Router} = require('express');
 const Order = require('../models/order');
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.render('orders', {
-    isOrder: true,
-    title: 'Orders'
-  });
+router.get('/', async (req, res) => {
+  try {
+    const orders = await Order.find({'user.userId': req.user._id}).populate('user.userId');
+
+    res.render('orders', {
+      isOrder: true,
+      title: 'Orders',
+      orders: orders.map(o => {
+        return {
+          ...o._doc,
+          price: o.courses.reduce((total, c) => {
+            return total += c.count * c.course.price
+          }, 0),
+        }
+      })
+    });
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 router.post('/', async (req, res) => {
