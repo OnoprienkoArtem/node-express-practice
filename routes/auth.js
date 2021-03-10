30eb78e5339c3e065a47feb92ab1d7231f55197f
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-10 22:10:39 +0200

    start implimenting email validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const {Router} = require('express');
 const bcrypt = require('bcryptjs');
 const crypto = require('crypto');
+const {body, validationResult} = require('express-validator/check');
 const nodemailer = require('nodemailer');
 const sendgrid = require('nodemailer-sendgrid-transport');
 const User = require('../models/user');
@@ -8,6 +9,7 @@ const keys = require('../keys');
 const regEmail = require('../emails/registration');
 const resetEmail = require('../emails/reset');
 const router = Router();
+
 
 const transporter = nodemailer.createTransport(sendgrid({
   auth: {api_key: keys.SENDGRID_API_KEY}
@@ -58,10 +60,16 @@ router.get('/logout', async (req, res) => {
   });
 });
 
-router.post('/register', async (req, res) => {
+router.post('/register', body('email').isEmail(), async (req, res) => {
   try {
-    const {email, password, repeat, name} = req.body;
+    const {email, password, confirm, name} = req.body;
     const candidate = await User.findOne({email});
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      req.flash('registerError', errors.array()[0].msg);
+      return res.status(422).redirect('/auth/login#register');
+    }
 
     if (candidate) {
       req.flash('registerError', 'User with this email already exists!');
