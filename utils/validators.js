54d4dd70fd095734673377304eed829d57eae2ef
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-12 22:53:57 +0200

    add sanitizer for form fields

--- utils/validators.js
+++ utils/validators.js
@@ -2,22 +2,31 @@ const {body, validationResult} = require('express-validator/check');
 const User = require('../models/user');
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Type correct email').custom(async (value, {req}) => {
-        try {
-            const user = await User.findOne({email: value});
-            if (user) {
-                return Promise.reject('This email already exists');
+    body('email')
+        .isEmail().withMessage('Type correct email')
+        .custom(async (value, {req}) => {
+            try {
+                const user = await User.findOne({email: value});
+                if (user) {
+                    return Promise.reject('This email already exists');
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
-        }
-    }),
-    body('password', 'Password should have min 6 symbols').isLength({min: 6, max: 56}).isAlphanumeric(),
-    body('confirm').custom((value, {req}) => {
-        if (value !== req.body.password) {
-            throw new Error('Passwords must match');
-        }
-        return true;
-    }),
-    body('name', 'Name should have min 3 symbols').isLength({min: 3}),
+        })
+        .normalizeEmail(),
+    body('password', 'Password should have min 6 symbols')
+        .isLength({min: 6, max: 56})
+        .isAlphanumeric()
+        .trim(),
+    body('confirm')
+        .custom((value, {req}) => {
+            if (value !== req.body.password) {
+                throw new Error('Passwords must match');
+            }
+            return true;
+        })
+        .trim(),
+    body('name', 'Name should have min 3 symbols').isLength({min: 3})
+        .trim(),
 ]
