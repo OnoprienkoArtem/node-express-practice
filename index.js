f0fa2f1df505db42712ed8afa659b5e3984f754e
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-16 22:27:26 +0200

    add profile page and route for it

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const addRoutes = require('./routes/add');
 const coursesRoutes = require('./routes/courses');
 const ordersRoutes = require('./routes/orders');
 const authRoutes = require('./routes/auth');
+const profileRoutes = require('./routes/profile');
 
 const varMiddleware = require('./middleware/variables');
 const userMiddleware = require('./middleware/user');
@@ -61,6 +62,7 @@ app.use('/courses', coursesRoutes);
 app.use('/cart', cartRoutes);
 app.use('/orders', ordersRoutes);
 app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
 
 app.use(errorHandler);
 
