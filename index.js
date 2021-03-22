a674ef04f6e1a88c10a1ba196923bceb0b48ed69
OnoprienkoArtem <o.artem.e@gmail.com>
2021-03-22 22:02:35 +0200

    setup helmet which set secure headers

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const MongoStore = require('connect-mongodb-session')(session);
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access');
+const helmet = require('helmet');
 
 const homeRoutes = require('./routes/home');
 const cartRoutes = require('./routes/cart');
@@ -56,6 +57,7 @@ app.use(session({
 app.use(fileMiddleware.single('avatar'));
 app.use(csrf());
 app.use(flash());
+app.use(helmet());
 app.use(varMiddleware);
 app.use(userMiddleware);
 
